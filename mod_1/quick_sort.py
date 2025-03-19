4e8f91f24e55884a3a08ca2ba3a1edce5630e6a8
Michał Kotra <michal.kotra2000@gmail.com>
2025-03-20 00:21:52 +0100

    20.03.2025

---
+++ mod_1/quick_sort.py
@@ -0,0 +1,45 @@
+def Lomuto_partition(A,p,r):
+    i = p-1
+    piwot = A[r]
+
+    for j in range(p,r):
+        if(A[j] <= piwot):
+            i+=1
+            A[i],A[j] = A[j],A[i]
+    A[r],A[i+1] = A[i+1],A[r]
+    return i
+
+def Hoare_partition(A,p,r):
+    i = p - 1
+    j = r + 1
+    piwot = A[(p+r)//2]
+    while(True):
+        i+=1
+        while(A[i] < piwot):
+            i+=1
+        j-=1
+        while(A[j] >piwot):
+            j-=1
+
+        if(i >= j):
+            return j
+        A[i],A[j] = A[j],A[i]
+
+    print(A[p:r] , " piwot: " , A[piwot])
+    return j
+
+def quick_sort(A,p,r):
+
+    if p < r:
+        q = Lomuto_partition(A,p,r)
+
+#IMPORTANT INFO!: if you are using Lomuto_partition you can change line below to quick_sort(A,p,q-1) for faster sorting
+
+        quick_sort(A,p,q)
+        quick_sort(A,q+1,r)
+
+
+
+tab = [15, 75, 41, 2, 25, 16, 77, 72, 37, 14, 62, 67, 93, 25, 87]
+quick_sort(tab,0,14)
+print(tab)
